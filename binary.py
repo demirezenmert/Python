73fa94b33c817b0630a85fb0c055fad247593489
Mert Demirezen <demirezenmert@gmail.com>
2020-03-20 02:49:18 +0300

    Fixing

---
+++ binary.py
@@ -0,0 +1,31 @@
+
+# #
+# #
+# # n logn max arama sayisini 
+
+
+
+# def binarySearch (arr, elem):
+    
+#     if len(arr) == 1 :
+#         return arr[0] == elem
+#     if len(arr) // 2 == elem :
+#         return True
+#     if len(arr) // 2 > elem :
+#         return binarySearch(arr[:len(arr)//2],elem) 
+#     else :
+#         return binarySearch(arr[len(arr)//2:],elem) 
+def ff (ar) :
+    if len(ar) == 0 :
+        print("by")
+    else :
+        print("debug")
+    
+testArray = [1,4]
+ff(testArray)
+print(testArray.sort())
+# testArray = testArray.sort()
+
+# print(binarySearch(testArray,6))
+
+
