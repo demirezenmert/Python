d502b08881b5517570906ba9e3b7d5bcc4385aef
Mert Demirezen <demirezenmert@gmail.com>
2020-03-22 02:05:15 +0300

    OnLy Lower Generator

--- generator_pass.py
+++ generator_pass.py
@@ -1,2 +1,13 @@
-import random 
+import random as rd
 
+def generatorPass (name) :
+    password = ""
+    name = name.lower()
+    lenght = rd.randint(6,10)
+    for  i in range(lenght) :
+        password += rd.choice(name)
+    return password
+
+name = input ("Please Enter Key of name :  ") 
+for i in range (10) : 
+    print(generatorPass(name))
