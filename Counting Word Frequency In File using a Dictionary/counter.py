c69f4908edd3d8fcd391b4da7ae653f0c8b4f9f5
Mert Demirezen <demirezenmert@gmail.com>
2020-03-23 21:09:22 +0300

    printing every frequecy words

--- Counting Word Frequency In File using a Dictionary/counter.py
+++ Counting Word Frequency In File using a Dictionary/counter.py
@@ -4,11 +4,19 @@ fname = input('Please Enter File: ')
 
 if len(fname) < 1 : fname = 'clown.txt'
 hand = open (fname)
-
+di = dict()
 for i in hand :
     lim = i.rstrip()
     # print(lim)
     wds = lim.split()
     # print(wds)
-    for word in wds :
-        print(word)
+    for w in wds :
+        print(w)
+        if w in di :
+            di[w] = di[w] + 1
+            print('***existing...')
+        else :
+            di[w] = 1
+            print('**** New')
+        print(di[w])
+print(di)
