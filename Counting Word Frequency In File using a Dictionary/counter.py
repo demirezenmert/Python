ff7a528920f24ac542668a30e347b92eb5fb6f98
Mert Demirezen <demirezenmert@gmail.com>
2020-03-23 21:41:25 +0300

    Counting with Dictionary Method

--- Counting Word Frequency In File using a Dictionary/counter.py
+++ Counting Word Frequency In File using a Dictionary/counter.py
@@ -11,12 +11,27 @@ for i in hand :
     wds = lim.split()
     # print(wds)
     for w in wds :
-        print(w)
-        if w in di :
-            di[w] = di[w] + 1
-            print('***existing...')
-        else :
-            di[w] = 1
-            print('**** New')
-        print(di[w])
+        # print(w)
+        # if w in di :
+        #     di[w] = di[w] + 1
+        #     print('***existing...')
+        # else :
+        #     di[w] = 1
+        #     print('**** New')
+        # print(di[w])
+        # ----- Second Method ------
+        # Dictionary Method -------
+
+
+        
+        # ----
+        # di.get(w,0) if w not exist count= 0
+        # ----
+
+        oldCount = di.get(w,0)
+        print(w,'Old',oldCount)
+        newCount = oldCount + 1
+        di[w] = newCount
+        print(w,"New",newCount)
+
 print(di)
