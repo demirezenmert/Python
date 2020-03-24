a07e095dd4859c97cf8e973138a15227ede8a0a0
Mert Demirezen <demirezenmert@gmail.com>
2020-03-25 01:34:39 +0300

    insertionAlgorithm DOne

--- SortingAlgorithm/insertionSorting.py
+++ SortingAlgorithm/insertionSorting.py
@@ -1,1 +1,27 @@
 #insertionSort 
+
+'''
+    Insertion Sorting Algortihm
+
+'''
+def insertionSort (collection):
+    
+    length = len(collection)
+
+    for i in range(1,length):
+        insertion_index = i 
+        while ( insertion_index > 0 
+        and collection[insertion_index-1]> collection[insertion_index]
+        ):
+            collection[insertion_index-1],collection[insertion_index]=collection[insertion_index],collection[insertion_index-1]
+            insertion_index -= 1
+    return collection
+
+
+
+
+if __name__ == "__main__":
+    user_input = input("Please Enter numbers separated by comma(,):\n").strip()
+    unsorted_collection = [int(items) for items in user_input.split(",")]
+    print(insertionSort(unsorted_collection))
+
